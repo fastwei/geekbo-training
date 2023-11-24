d9607a44243688ed6e60bd3e359ed27d7959279b
geekbo <runcodeman@gmail.com>
2023-11-24 09:38:21 +0800

    crack leecode

---
+++ src/main/java/com/geekbo/training/leetcode/daily/CountPairsWhoseSumLessThanTarget.java
@@ -0,0 +1,76 @@
+package com.geekbo.training.leetcode.daily;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 2824. Count Pairs Whose Sum is Less than Target
+ * Easy
+ * Given a 0-indexed integer array nums of length n and an integer target,
+ * return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+ * Example 1:
+ * <p>
+ * Input: nums = [-1,1,2,3,1], target = 2
+ * Output: 3
+ * Explanation: There are 3 pairs of indices that satisfy the conditions in the statement:
+ * - (0, 1) since 0 < 1 and nums[0] + nums[1] = 0 < target
+ * - (0, 2) since 0 < 2 and nums[0] + nums[2] = 1 < target
+ * - (0, 4) since 0 < 4 and nums[0] + nums[4] = 0 < target
+ * Note that (0, 3) is not counted since nums[0] + nums[3] is not strictly less than the target.
+ * Example 2:
+ * <p>
+ * Input: nums = [-6,2,5,-2,-7,-1,3], target = -2
+ * Output: 10
+ * Explanation: There are 10 pairs of indices that satisfy the conditions in the statement:
+ * - (0, 1) since 0 < 1 and nums[0] + nums[1] = -4 < target
+ * - (0, 3) since 0 < 3 and nums[0] + nums[3] = -8 < target
+ * - (0, 4) since 0 < 4 and nums[0] + nums[4] = -13 < target
+ * - (0, 5) since 0 < 5 and nums[0] + nums[5] = -7 < target
+ * - (0, 6) since 0 < 6 and nums[0] + nums[6] = -3 < target
+ * - (1, 4) since 1 < 4 and nums[1] + nums[4] = -5 < target
+ * - (3, 4) since 3 < 4 and nums[3] + nums[4] = -9 < target
+ * - (3, 5) since 3 < 5 and nums[3] + nums[5] = -3 < target
+ * - (4, 5) since 4 < 5 and nums[4] + nums[5] = -8 < target
+ * - (4, 6) since 4 < 6 and nums[4] + nums[6] = -4 < target
+ */
+class CountPairsWhoseSumLessThanTarget {
+
+    /**
+     * 解题思路：
+     * <p>
+     * 首先对数组进行排序，以便使用双指针来查找符合条件的数对。
+     * 使用两个指针left和right，初始时分别指向数组的最左端和最右端。
+     * 通过计算nums[left] + nums[right]的值，判断是否满足条件。如果和小于目标值target，则说明左指针和右指针之间的所有数对的和都小于target，所以可以将左指针向右移动一位，并将计数器count增加right - left。
+     * 如果和大于等于目标值target，则将右指针向左移动一位。
+     * 重复上述步骤直到左指针大于等于右指针。
+     * <p>
+     * <p>
+     * 时间复杂度：排序数组的时间复杂度是 O(n log n)，而双指针遍历数组的时间复杂度是 O(n)，所以总体时间复杂度是 O(n log n)。
+     * 空间复杂度：排序算法的空间复杂度是 O(log n)，而双指针的空间复杂度是 O(1)，所以总体空间复杂度是 O(log n)。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int countPairs(List<Integer> nums, int target) {
+        Collections.sort(nums); // sort the vector nums
+        int count = 0; // variable to store the count
+        int left = 0; // variable to store the left
+        int right = nums.size()-1; // variable to store the right
+        while(left < right){ // loop until left is less than right
+            if(nums.get(left) + nums.get(right) < target){ // if nums[left] + nums[right] is less than target
+                count += right-left; // update the count
+                left++; // increment the left
+            }
+            else{ // else
+                right--; // decrement the right
+            }
+        }
+        return count; // return the count
+
+    }
+
+}
+
+
