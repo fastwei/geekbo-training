9ba7a2831fee4c94c0cdd2dac973e1ad24e1172e
geekbo <runcodeman@gmail.com>
2023-12-25 00:10:15 +0800

    crack leecode

--- src/main/java/com/geekbo/training/leetcode/premium/UndergroundSystem.java
+++ src/main/java/com/geekbo/training/leetcode/premium/UndergroundSystem.java
@@ -1,5 +1,8 @@
 package com.geekbo.training.leetcode.premium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * 1396. 设计地铁系统
@@ -94,8 +97,3 @@ class UndergroundSystem {
         return 1.0 * sum / amount;
     }
 }
-
-作者：力扣官方题解
-链接：https://leetcode.cn/problems/design-underground-system/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
